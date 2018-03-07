30001ad39e15069527f48211c4506aebe128213e
YanC Lin <yonalin1@gmail.com>
2018-03-07 18:43:50 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,24 +1,24 @@
 class Show < ActiveRecord::Base
-
-def highest_rating
-Show.maximum(:rating)
-end
-def most_popular_show
-  Show.where(highest_rating)
-end
-def lowest_rating
-  Show.minimum(:rating)
-end
-def least_popular_show
-  Show.where(lowest_rating)
-end
-def ratings_sum
-  Show.sum(:rating)
-end
-def popular_shows
-  Show.where("rating > ?", 5)
-end
-def shows_by_alphabetical_order
-  Show.order(created_at: :asc)
-end
+  
+  def highest_rating
+    Show.maximum(:rating)
+  end
+  def most_popular_show
+    Show.where(highest_rating)
+  end
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+  def least_popular_show
+    Show.where(lowest_rating)
+  end
+  def ratings_sum
+    Show.sum(:rating)
+  end
+  def popular_shows
+    Show.where("rating > ?", 5)
+  end
+  def shows_by_alphabetical_order
+    Show.order(created_at: :asc)
+  end
 end
