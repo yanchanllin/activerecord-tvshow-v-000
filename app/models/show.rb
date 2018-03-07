d749e90d5f8707f36005defbfb96a20112efdf71
YanC Lin <yonalin1@gmail.com>
2018-03-07 19:02:01 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,7 +10,7 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end
   def self.least_popular_show
-    Show.where(self.lowest_rating:true)
+    Show.where(self.lowest_rating:self.lowest_rating)
   end
   def self.ratings_sum
     Show.sum(:rating)
