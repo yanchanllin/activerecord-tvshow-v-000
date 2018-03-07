52e902cb0b7f360192dd797f978dbaee04c032df
YanC Lin <yonalin1@gmail.com>
2018-03-07 17:20:16 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
-class CreateShows < ActiveRecord::Migration 
-  def change 
+class CreateShows < ActiveRecord::Migration
+  def change
     create_table :shows do |t|
-      t.string :name 
+      t.string :name
       t.string :network
       t.string :day
       t.integer :rating
     end
-  end   
+  end
 end 
