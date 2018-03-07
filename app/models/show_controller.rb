c6e6ecb6105505c2b49f5effebb5108d6e187a73
YanC Lin <yonalin1@gmail.com>
2018-03-07 18:38:27 +0000

    automatically backed up by learn

--- app/models/show_controller.rb
+++ app/models/show_controller.rb
@@ -1,2 +1,21 @@
 def highest_rating
+Show.maximum(:rating)
 end
+def most_popular_show
+  Show.where(highest_rating)
+end 
+def lowest_rating
+  Show.minimum(:rating)
+end 
+def least_popular_show
+  Show.where(lowest_rating)
+end 
+def ratings_sum
+  Show.sum(:rating)
+end 
+def popular_shows
+  Show.where("rating > ?", 5)
+end 
+def shows_by_alphabetical_order
+  Show.order(:create_at ASC)
+end 
