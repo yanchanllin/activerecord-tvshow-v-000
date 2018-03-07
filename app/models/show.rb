4b24cc0dbadabbaac26a60844fa68a98b502e0b5
YanC Lin <yonalin1@gmail.com>
2018-03-07 19:09:14 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
   def self.most_popular_show
-    Show.where("self.highest_rating = ?", params[:orders])
+    Show.where(self.highest_rating.first)
   end
   def self.lowest_rating
     Show.minimum(:rating)
